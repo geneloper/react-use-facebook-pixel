21d815140b033af1d5c80807b794f20532143b8c
Eugene Medvedev <eugenemedvedevbelarus@gmail.com>
2025-04-21 23:55:32 +0300

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      outDir: 'dist',
+      include: ['src/**/*'],
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'ReactUseFacebookPixel',
+      name: 'FacebookPixel',
       fileName: (format) => `index.${format === 'es' ? 'esm' : 'cjs'}.js`,
+      formats: ['es', 'cjs'],
     },
     sourcemap: true,
+    rollupOptions: {
+      external: [],
+    },
   },
 });
